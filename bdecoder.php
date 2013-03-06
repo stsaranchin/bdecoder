c0209c9386f3085230ce6f30d831e0182be9ae66
Stanislav Saranchin <stanislavdrew@gmail.com>
2013-03-06 16:23:55 +1100

    Some code improvements

--- bdecoder.php
+++ bdecoder.php
@@ -29,57 +29,52 @@ class BDecoder {
 		return $sum;
 	}
 
-	private static function _parse_list($str, $level) {
+	private static function _parse_list($str) {
 		$_list = array();
 
 		$str = substr($str, 1);	
 
-		while ($str[0] != 'e' && ($item = self::parse($str, ++$level)) !== false) {
+		while ($str[0] != 'e' && ($item = self::parse($str)) !== false) {
 			$_list[] = $item;
 		
 			$str = substr($str, self::_sum_all($item));
 		}
 
-		if (count($_list) == 0) {
-			$_list = array(null);
-		}
-
 		return (array) $_list;		
 	}
 
-	public static function parse($str, $level = 0) {
+	public static function parse($str) {
 		switch ($str[0]) {
 			case 'l':
-				return self::_parse_list($str, $level);
+				return self::_parse_list($str);
 
 			case 'd':
 				$_dict = array();
 
-				$_list = self::_parse_list($str, $level);
+				$_list = self::_parse_list($str);
 
 				if (count($_list) == 0) {
-					$_dict = array(null);
+					return array();
+				}
 
-				} else {
-					foreach (array_chunk($_list, 2) as $key) {
-						$_dict[$key[0]] = $key[1];
-					}
+				foreach (array_chunk($_list, 2) as $key) {
+					$_dict[$key[0]] = $key[1];
 				}
 
 				return (array) $_dict;
 
 			case 'i':
 				$marker = strpos($str, 'e');
-				$number = (int) substr($str, 1, $marker - 1);
+				$number = substr($str, 1, $marker - 1);
 
-				return $number;
+				return (int) $number;
 
 			case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
 				$marker = strpos($str, ':');
 				$length = substr($str, 0, $marker);
 				$data = substr($str, $marker + 1, $length);
 
-				return $data;
+				return (string) $data;
 
 			default:
 				return false;
--- decode.php
+++ decode.php
@@ -2,6 +2,4 @@
 
 include_once('bdecoder.php');
 
-//var_dump(BDecoder::parse(file_get_contents('resume.dat')));
-
-var_dump(BDecoder::parse('d2:ani0e5:applei0e6:sixsixli0ei0ei0eee'));
+var_dump(BDecoder::parse(file_get_contents('resume.dat')));
